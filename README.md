92443fe89b78bdb3b2887b41d1def31cb3da5735
Idrias <nomail1@gmx.net>
2016-03-23 21:34:57 +0100

    0.9

---
+++ README.md
@@ -0,0 +1,3 @@
+It is easy. You can look at the example script if you want to find out how it works.
+
+Don't worry. I'll add a nice Readme with version 1.0 once I upload it :-)
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'easynet',
+  packages = ['easynet'],
+  version = '0.9',
+  description = 'A collection of scripts that try to help beginners with an easy network interface',
+  author = 'Idrias',
+  author_email = 'nomail1@gmx.net',
+  url = 'https://github.com/peterldowns/mypackage',
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+  keywords = ['network', 'connect', 'server'],
+  classifiers = [],
+)
